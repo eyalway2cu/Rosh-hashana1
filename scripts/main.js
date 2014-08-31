4818102fce9676da726de0ce85dbce7ffc8e7ab2
Mladen Mijatov <meaneye.rcf@gmail.com>
2014-08-31 19:43:14 +0300

    Added Tranzilla popup.

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,44 @@
  * Authors:
  */
 
+/**
+ * Show tranzila dialog.
+ */
+function showTranzilla(event) {
+	event.preventDefault();
+
+	var form = $(this);
+	var data = {};
+	var iframe = $('<iframe>');
+	var url = 'https://direct.tranzila.com/<account>/iframe.php?';
+
+	// prepare params for tranzilla
+	var params = {
+		lang: 'he',
+		sum: 100,
+		cred_type: 8,
+		currency: 1
+	};
+
+	// configure iframe
+	iframe
+		.attr('id', 'tranzila')
+		.attr('frameborder', 0)
+		.attr('src', url + $.param(params))
+		.appendTo($('body'));
+
+	// show iframe
+	iframe.addClass('visible');
+
+	if (error)
+		var response = true; else
+		var response = false;
+
+	return response;
+}
+
 function on_site_load() {
+	$('a.donate_now').click(showTranzilla);
 }
 
 $(on_site_load);
